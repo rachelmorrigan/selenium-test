fc3c89748f20560de179aef21ba1a31cf2be3afe
rachelmorrigan <rachelmorrigan@mail.ru>
2021-05-29 21:30:38 +0300

    add products and sideBar tests

--- src/test/java/litecart/LoginTests.java
+++ src/test/java/litecart/LoginTests.java
@@ -1,17 +1,23 @@
 package litecart;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LoginTests {
     private WebDriver webDriver;
     private WebDriverWait wait;
 
-    @BeforeTest
+    @BeforeClass
     public void beforeTest(){
         webDriver = new ChromeDriver();
         wait = new WebDriverWait(webDriver, 10);
@@ -26,7 +32,25 @@ public class LoginTests {
         wait.until(webDriver -> ((JavascriptExecutor)webDriver).executeScript("return document.readyState").equals("complete"));
     }
 
-    @AfterTest
+    @Test(dependsOnMethods = "positiveLoginTest" )
+    public void checkSideBar(){
+        List<String> names = webDriver.findElements(By.xpath("//ul[@id='box-apps-menu']/li/a/span[@class='name']")).stream().map(WebElement::getText).collect(Collectors.toList());
+        names.forEach(name -> {
+           WebElement webElement = webDriver.findElement(By.xpath("//ul[@id='box-apps-menu']/li/a/span[@class='name' and .='" + name + "']/.."));
+           webElement.click();
+           WebElement selectedWebElement = webDriver.findElement(By.xpath("//ul[@id='box-apps-menu']/li[@class='selected']"));
+           assert webDriver.findElements(By.cssSelector("h1")).size() != 0 : "На странице нет элемента h1";
+           List<String> subNames = selectedWebElement.findElements(By.xpath("//ul[@class='docs']/li[not(contains(@class,'selected'))]"))
+                   .stream().map(WebElement::getText).collect(Collectors.toList());
+           subNames.forEach(subName -> {
+               WebElement subElement = webDriver.findElement(By.xpath("//ul[@id='box-apps-menu']/li[@class='selected']/ul[@class='docs']/li/a/span[@class='name' and .='" + subName + "']/.."));
+               subElement.click();
+               assert webDriver.findElements(By.cssSelector("h1")).size() != 0 : "На странице нет элемента h1";
+           });
+        });
+    }
+
+    @AfterClass
     public void afterTest(){
         webDriver.quit();
         webDriver = null;
---
+++ src/test/java/litecart/MainPageTests.java
@@ -0,0 +1,35 @@
+package litecart;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class MainPageTests {
+    private WebDriver webDriver;
+
+    @BeforeTest
+    public void beforeTest(){
+        webDriver = new FirefoxDriver();
+    }
+
+    @Test
+    public void checkProducts(){
+        webDriver.get("http://localhost/litecart/");
+        webDriver.findElements(By.cssSelector("li[class='product column shadow hover-light']")).forEach(webElement -> {
+            List<WebElement> webElements = webElement.findElements(By.cssSelector("a div.image-wrapper div[class ^=sticker]"));
+            assert webElements.size() == 1 : "Кол-во стикеров на товаре не равно 1";
+        });
+    }
+
+    @AfterTest
+    public void afterTest(){
+        webDriver.quit();
+        webDriver = null;
+    }
+}
